f922c4b3fc397c110e45e565bf0ccf0b8c6ae45e
Taha Biklaryan <taha.biglarian@gmail.com>
2025-12-07 21:56:55 +0330

    hotfix: added role of admin in UserInfoResponse dto

--- gateway/internal/application/dto/auth/response.go
+++ gateway/internal/application/dto/auth/response.go
@@ -4,4 +4,5 @@ type UserInfoResponse struct {
 	AccessToken   string `json:"access_token"`
 	RefereshToken string `json:"refresh_token"`
 	PhoneNumber   string `json:"phone_number"`
+	IsAdmin       bool   `json:"is_admin"`
 }
