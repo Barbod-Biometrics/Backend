e0955e95037b156f38a0af415722178bef20cce9
nimasdi <ni.saeidi83@gmail.com>
2025-12-18 20:19:34 +0330

    feat: created the structure of repo and service

---
+++ gateway/internal/application/dto/face_verification/face_verification.go
@@ -0,0 +1,37 @@
+package face_verification
+
+type FaceVerificationRequest struct {
+	Photo []byte `json:"photo" form:"photo"`
+	Video []byte `json:"video" form:"video"`
+}
+
+type FaceVerificationResponse struct {
+	Success  bool        `json:"success"`
+	Reason   string      `json:"reason"`
+	Message  string      `json:"message"`
+	Stats    interface{} `json:"stats,omitempty"`
+	Results  interface{} `json:"results,omitempty"`
+	Messages interface{} `json:"messages,omitempty"`
+}
+
+type CropImageRequest struct {
+	Image []byte `json:"image" form:"image"`
+}
+
+type CropImageResponse struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+	Error   string `json:"error,omitempty"`
+	Image   []byte `json:"image,omitempty"`
+}
+
+type HealthCheckRequest struct {
+	// no fields
+}
+
+type HealthCheckResponseDTO struct {
+	Status  string      `json:"status"`
+	Message string      `json:"message"`
+	Models  interface{} `json:"models,omitempty"`
+	Config  interface{} `json:"config,omitempty"`
+}
---
+++ gateway/internal/application/usecase/face_verification_usecase.go
@@ -0,0 +1,15 @@
+package usecase
+
+import (
+	"context"
+
+	faceVerificationDto "github.com/Barbod-Biometrics/Backend/gateway/internal/application/dto/face_verification"
+)
+
+type FaceVerificationUsecase interface {
+	VerifyFace(ctx context.Context, profileID uint64, photo []byte, video []byte) (*faceVerificationDto.FaceVerificationResponse, error)
+
+	CropImage(ctx context.Context, image []byte) (*faceVerificationDto.CropImageResponse, error)
+
+	HealthCheck(ctx context.Context) (*faceVerificationDto.HealthCheckResponseDTO, error)
+}
