39c60d1fff3b57336307739671619f0aebe64828
nimasdi <ni.saeidi83@gmail.com>
2025-12-20 18:13:31 +0330

    feat: created the base structure

---
+++ gateway/internal/application/dto/ocr/ocr.go
@@ -0,0 +1,26 @@
+package ocr
+
+
+type OCRRequest struct {
+	Image string `json:"image" binding:"required"` 
+}
+
+type OCRResponse struct {
+	Success bool                   `json:"success"`
+	Message string                 `json:"message,omitempty"`
+	Stats   map[string]interface{} `json:"stats,omitempty"`
+
+	NationalID     string `json:"شماره_ملی,omitempty"`
+	FirstName      string `json:"نام,omitempty"`
+	LastName       string `json:"نام_خانوادگی,omitempty"`
+	FatherName     string `json:"نام_پدر,omitempty"`
+	BirthDate      string `json:"تاریخ_تولد,omitempty"`
+	ExpirationDate string `json:"پایان_اعتبار,omitempty"`
+}
+
+type HealthCheckResponseDTO struct {
+	Status               string  `json:"status"`
+	ProcessingTimeSecond float64 `json:"processing_time_seconds,omitempty"`
+}
+
+
---
+++ gateway/internal/application/usecase/ocr_usecase.go
@@ -0,0 +1,13 @@
+package usecase
+
+import (
+	"context"
+
+	ocrDto "github.com/Barbod-Biometrics/Backend/gateway/internal/application/dto/ocr"
+)
+
+type OCRUsecase interface {
+	ExtractText(ctx context.Context, profileID uint64, image []byte) (*ocrDto.OCRResponse, error)
+
+	HealthCheck(ctx context.Context) (*ocrDto.HealthCheckResponseDTO, error)
+}
